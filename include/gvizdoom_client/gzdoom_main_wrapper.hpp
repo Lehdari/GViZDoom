0132f6f1b24ab7287032ee0c7446072579a0192e
Lehdari <miika.lehtimaki@gmail.com>
2022-11-09 01:57:18 +0200

    gvizdoom_client: Add gzdoom_main_wrapper.hpp/cpp
    gzdoom_main_wrapper simply wraps the SDL-specific launch main function (or rather copies it) to enable gvizdoom to be used as a shared library. These files are to be removed upon completion of the SDL code migration.

---
+++ include/gvizdoom_client/gzdoom_main_wrapper.hpp
@@ -0,0 +1,16 @@
+//
+// Project: GViZDoom
+// File: gzdoom_main_wrapper.hpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+// TODO this file is to be removed once SDL stuff is successfully migrated out of gvizdoom
+
+#pragma once
+
+
+int gzdoom_main_wrapper(int argc, char **argv);
