497e2f37987d3a8728fcf23d9fe7a36683b26ae8
Lehdari <miika.lehtimaki@gmail.com>
2022-11-08 19:48:00 +0200

    Add DoomGame class

---
+++ include/gvizdoom/DoomGame.hpp
@@ -0,0 +1,24 @@
+//
+// Project: GViZDoom
+// File: DoomGame.hpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#pragma once
+
+
+namespace gvizdoom {
+
+    class DoomGame {
+    public:
+        void init();
+
+    private:
+
+    };
+
+}
---
+++ src/gvizdoom/DoomGame.cpp
@@ -0,0 +1,20 @@
+//
+// Project: GViZDoom
+// File: DoomGame.cpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "gvizdoom/DoomGame.hpp"
+
+
+using namespace gvizdoom;
+
+
+void DoomGame::init()
+{
+
+}
