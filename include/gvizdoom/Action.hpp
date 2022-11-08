edae5493903a12d2bc6583ee6301837abf9817eb
Lehdari <miika.lehtimaki@gmail.com>
2022-11-08 20:01:58 +0200

    Add Action class

---
+++ include/gvizdoom/Action.hpp
@@ -0,0 +1,21 @@
+//
+// Project: GViZDoom
+// File: Action.hpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#pragma once
+
+
+namespace gvizdoom {
+
+// Action class encapsulates all information required for a game state update
+class Action {
+    // TODO
+};
+
+} // namespace gvizdoom
