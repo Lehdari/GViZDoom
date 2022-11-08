c88da28162c8c79595b1ab08bfb3b9da0bd9d961
Lehdari <miika.lehtimaki@gmail.com>
2022-11-08 21:10:48 +0200

    Add gvizdoom_client executable
    gvizdoom_client will facilitate an user interface (window and user input handling) for interactive debugging purposes.

---
+++ src/gvizdoom_client/main.cpp
@@ -0,0 +1,63 @@
+//
+// Project: GViZDoom
+// File: main.cpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "gvizdoom_client/App.hpp"
+
+
+using namespace gvizdoom;
+
+
+// Pipeline function for the event handling
+void handleEvents(SDL_Event& event, App::Context& appContext)
+{
+    // Handle SDL events
+    switch (event.type) {
+        case SDL_QUIT:
+            *appContext.quit = true;
+            break;
+
+        case SDL_KEYDOWN:
+            // Skip events if imgui widgets are being modified
+            switch (event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    *appContext.quit = true;
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+// Pipeline function for rendering
+void render(RenderContext& renderContext, App::Context& appContext)
+{
+    
+}
+
+
+int main(int argv, char** args)
+{
+    // Setup app and render context
+    App::Settings appSettings;
+    appSettings.handleEvents = &handleEvents;
+    appSettings.render = &render;
+    RenderContext context;
+    App app(appSettings);
+    app.setRenderContext(&context);
+
+    app.loop();
+
+    return 0;
+}
