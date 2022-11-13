a76942345be543d177daab06bff7117d1928d5f7
Eljas Hyyrynen <eljas.hyyrynen@gmail.com>
2022-11-13 16:40:45 +0200

    change tabs to spaces in d_main.h

--- src/doom/d_main.h
+++ src/doom/d_main.h
@@ -83,40 +83,38 @@ extern uint32_t r_renderercaps;
 
 struct FIWADInfo
 {
-	FString Name;			// Title banner text for this IWAD
-	FString Autoname;		// Name of autoload ini section for this IWAD
-	FString IWadname;		// Default name this game would use - this is for IWAD detection in GAMEINFO.
-	int prio = 0;			// selection priority for given IWAD name.
-	FString Configname;		// Name of config section for this IWAD
-	FString Required;		// Requires another IWAD
-	uint32_t FgColor = 0;	// Foreground color for title banner
-	uint32_t BkColor = 0xc0c0c0;		// Background color for title banner
-	FString Song;
-	EGameType gametype = GAME_Doom;		// which game are we playing?
-	int StartupType = FStartupInfo::DefaultStartup;		// alternate startup type
-	FString MapInfo;		// Base mapinfo to load
-	bool nokeyboardcheats = false;		// disable keyboard cheats
-	TArray<FString> Load;	// Wads to be loaded with this one.
-	TArray<FString> Lumps;	// Lump names for identification
-	TArray<FString> DeleteLumps;	// Lumps which must be deleted from the directory.
-	int flags = 0;
-	int LoadWidescreen = -1;
-	int LoadBrightmaps = -1;
-	int LoadLights = -1;
-	FString SteamAppId = nullptr;
+    FString Name;                                   // Title banner text for this IWAD
+    FString Autoname;                               // Name of autoload ini section for this IWAD
+    FString IWadname;                               // Default name this game would use - this is for IWAD detection in GAMEINFO.
+    int prio = 0;                                   // selection priority for given IWAD name.
+    FString Configname;                             // Name of config section for this IWAD
+    FString Required;                               // Requires another IWAD
+    uint32_t FgColor = 0;                           // Foreground color for title banner
+    uint32_t BkColor = 0xc0c0c0;                    // Background color for title banner
+    FString Song;
+    EGameType gametype = GAME_Doom;                 // which game are we playing?
+    int StartupType = FStartupInfo::DefaultStartup; // alternate startup type
+    FString MapInfo;                                // Base mapinfo to load
+    bool nokeyboardcheats = false;                  // disable keyboard cheats
+    TArray<FString> Load;                           // Wads to be loaded with this one.
+    TArray<FString> Lumps;                          // Lump names for identification
+    TArray<FString> DeleteLumps;                    // Lumps which must be deleted from the directory.
+    int flags = 0;
+    int LoadWidescreen = -1;
+    int LoadBrightmaps = -1;
+    int LoadLights = -1;
+    FString SteamAppId = nullptr;
 };
 
 struct FFoundWadInfo
 {
-	FString mFullPath;
-	FString mRequiredPath;
-	int mInfoIndex = -1;	// must be an index because of reallocation
+    FString mFullPath;
+    FString mRequiredPath;
+    int mInfoIndex = -1;	// must be an index because of reallocation
 
-	FFoundWadInfo() {}
-	FFoundWadInfo(const FString &s1, const FString &s2, int index)
-		: mFullPath(s1), mRequiredPath(s2), mInfoIndex(index)
-	{
-	}
+    FFoundWadInfo() {}
+    FFoundWadInfo(const FString &s1, const FString &s2, int index)
+        : mFullPath(s1), mRequiredPath(s2), mInfoIndex(index) {}
 };
 
 //==========================================================================
@@ -127,58 +125,56 @@ struct FFoundWadInfo
 
 class FIWadManager
 {
-	TArray<FIWADInfo> mIWadInfos;
-	TArray<FString> mIWadNames;
-	TArray<FString> mSearchPaths;
-	TArray<FString> mOrderNames;
-	TArray<FFoundWadInfo> mFoundWads;
-	TArray<int> mLumpsFound;
+    TArray<FIWADInfo> mIWadInfos;
+    TArray<FString> mIWadNames;
+    TArray<FString> mSearchPaths;
+    TArray<FString> mOrderNames;
+    TArray<FFoundWadInfo> mFoundWads;
+    TArray<int> mLumpsFound;
 
-	void ParseIWadInfo(const char *fn, const char *data, int datasize, FIWADInfo *result = nullptr);
-	int ScanIWAD (const char *iwad);
-	int CheckIWADInfo(const char *iwad);
-	int IdentifyVersion (TArray<FString> &wadfiles, const char *iwad, const char *zdoom_wad, const char *optional_wad);
-	void CollectSearchPaths();
-	void AddIWADCandidates(const char *dir);
-	void ValidateIWADs();
+    void ParseIWadInfo(const char *fn, const char *data, int datasize, FIWADInfo *result = nullptr);
+    int ScanIWAD (const char *iwad);
+    int CheckIWADInfo(const char *iwad);
+    int IdentifyVersion (TArray<FString> &wadfiles, const char *iwad, const char *zdoom_wad, const char *optional_wad);
+    void CollectSearchPaths();
+    void AddIWADCandidates(const char *dir);
+    void ValidateIWADs();
 public:
-	FIWadManager(const char *fn, const char *fnopt);
-	const FIWADInfo *FindIWAD(TArray<FString> &wadfiles, const char *iwad, const char *basewad, const char *optionalwad);
-	const FString *GetAutoname(unsigned int num) const
-	{
-		if (num < mIWadInfos.Size()) return &mIWadInfos[num].Autoname;
-		else return NULL;
-	}
-	int GetIWadFlags(unsigned int num) const
-	{
-		if (num < mIWadInfos.Size()) return mIWadInfos[num].flags;
-		else return 0;
-	}
-
-
+    FIWadManager(const char *fn, const char *fnopt);
+    const FIWADInfo *FindIWAD(TArray<FString> &wadfiles, const char *iwad, const char *basewad, const char *optionalwad);
+    const FString *GetAutoname(unsigned int num) const
+    {
+        if (num < mIWadInfos.Size()) return &mIWadInfos[num].Autoname;
+        else return NULL;
+    }
+    
+    int GetIWadFlags(unsigned int num) const
+    {
+        if (num < mIWadInfos.Size()) return mIWadInfos[num].flags;
+        else return 0;
+    }
 };
 
 struct D_DoomMain_Internal_State {
-    const char *v;
-    const char *wad;
-    FIWadManager *iwad_man;
-
-    FString basewad;
-    FString optionalwad;
-
-    int lasttic;
+    const char *v{};
+    const char *wad{};
+    FIWadManager *iwad_man{};
+    FString basewad{};
+    FString optionalwad{};
+    int lasttic{0};		   // Last tick index
+    bool singletics{true}; // Run single or multiple tics at the same time?
 };
 
 EXTERN_CVAR(Int, vid_rendermode)
 
 inline bool V_IsHardwareRenderer()
 {
-	return vid_rendermode == 4;
+    return vid_rendermode == 4;
 }
 
 inline bool V_IsTrueColor()
 {
-	return vid_rendermode == 1 || vid_rendermode == 4;
+    return vid_rendermode == 1 || vid_rendermode == 4;
 }
 
 bool CheckCheatmode(bool printmsg = true, bool sponly = false);
