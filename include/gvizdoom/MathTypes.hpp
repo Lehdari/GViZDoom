a104fd460eabdc2961ceeb7329f15cd8e29f8bad
Eljas Hyyrynen <eljas.hyyrynen@gmail.com>
2023-01-02 17:52:39 +0200

    Add level exit to GameState

---
+++ include/gvizdoom/MathTypes.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <Eigen/Dense>
+
+namespace gvizdoom
+{
+
+using Vec2f = Eigen::Matrix<float, 2, 1>;
+using Vec3f = Eigen::Matrix<float, 3, 1>;
+using Vec4f = Eigen::Matrix<float, 4, 1>;
+
+using Vec2d = Eigen::Matrix<double, 2, 1>;
+using Vec3d = Eigen::Matrix<double, 3, 1>;
+using Vec4d = Eigen::Matrix<double, 4, 1>;
+
+using Mat2f = Eigen::Matrix<float, 2, 2>;
+using Mat3f = Eigen::Matrix<float, 3, 3>;
+using Mat4f = Eigen::Matrix<float, 4, 4>;
+
+using Mat2d = Eigen::Matrix<double, 2, 2>;
+using Mat3d = Eigen::Matrix<double, 3, 3>;
+using Mat4d = Eigen::Matrix<double, 4, 4>;
+
+} // namespace gvizdoom
